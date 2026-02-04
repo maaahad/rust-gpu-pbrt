782210b96c15338944d6593d9438eb2351da98fd
Muhammed Ahad <maaahad@gmail.com>
2026-02-04 23:15:49 +0100

    State struct with placehodler methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::sync::Arc; 
+use winit::{
+    application::ApplicationHandler, event::*, event_loop::{ActiveEventLoop, EventLoop}, keyboard::{KeyCode, PhysicalKey}, window::Window
+}; 
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*; 
+
+pub struct State {
+    window: Arc<Window>,
+}
+
+impl State {
+    pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        Ok(Self{window})
+    }
+
+    pub fn resize(&mut self, _width: u32, _height: u32) {
+        println!("Resize : window resize"); 
+    }
+
+    pub fn render(&mut self) {
+        self.window.request_redraw(); 
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
