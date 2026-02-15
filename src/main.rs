1125a117e487ddd0955bed40f3a12e93a6c7f075
Muhammed Ahad <maaahad@gmail.com>
2026-02-15 11:15:55 +0100

    format

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
-extern crate glfw; 
-use glfw::{fail_on_errors, Action, Context, Key }; 
+extern crate glfw;
+use glfw::{Action, Context, Key, fail_on_errors};
 
 fn main() {
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
 
-    let mut glfw = glfw::init(fail_on_errors!()).unwrap(); 
-
-    let (mut window, events) = glfw.create_window(400, 400, "GLFW Window", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW Window"); 
+    let (mut window, events) = glfw
+        .create_window(400, 400, "GLFW Window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW Window");
 
     window.make_current();
-    window.set_key_polling(true); 
+    window.set_key_polling(true);
 
     while !window.should_close() {
-        window.swap_buffers(); 
-        glfw.poll_events(); 
+        window.swap_buffers();
+        glfw.poll_events();
 
         for (_, event) in glfw::flush_messages(&events) {
-            println!("event: {:?}", event); 
+            println!("event: {:?}", event);
 
             match event {
-                glfw::WindowEvent::Key(Key::Escape, _, Action::Press,_)  => {
-                    window.set_should_close(true); 
-                }, 
-                _ => {}, 
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true);
+                }
+                _ => {}
             }
         }
     }
