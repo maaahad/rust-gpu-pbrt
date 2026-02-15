eb9875d22685dc3f37ba0f7bb00cbd5d07978617
Muhammed Ahad <maaahad@gmail.com>
2026-02-15 01:01:17 +0100

    glfw window init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate glfw; 
+use glfw::{fail_on_errors, Action, Context, Key }; 
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap(); 
+
+    let (mut window, event) = glfw.create_window(400, 400, "GLFW Window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW Window"); 
+
+    window.make_current(); 
+    window.set_key_polling(true); 
+
+    while !window.should_close() {
+        window.swap_buffers(); 
+        glfw.poll_events(); 
+    }
 }
