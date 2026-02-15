a142ec086bd82c4d323b0551683e008931c32637
Muhammed Ahad <maaahad@gmail.com>
2026-02-15 11:01:29 +0100

    escape key event

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,25 @@ fn main() {
 
     let mut glfw = glfw::init(fail_on_errors!()).unwrap(); 
 
-    let (mut window, event) = glfw.create_window(400, 400, "GLFW Window", glfw::WindowMode::Windowed)
+    let (mut window, events) = glfw.create_window(400, 400, "GLFW Window", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW Window"); 
 
-    window.make_current(); 
+    window.make_current();
     window.set_key_polling(true); 
 
     while !window.should_close() {
         window.swap_buffers(); 
         glfw.poll_events(); 
+
+        for (_, event) in glfw::flush_messages(&events) {
+            println!("event: {:?}", event); 
+
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press,_)  => {
+                    window.set_should_close(true); 
+                }, 
+                _ => {}, 
+            }
+        }
     }
 }
