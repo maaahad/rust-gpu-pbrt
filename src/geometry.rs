e8d749eb4886b7c9cdbf5d553728639a7e2360d4
Muhammed Ahad <maaahad@gmail.com>
2026-03-07 19:27:53 +0100

    index opertor for Vector3

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 #[derive(Debug)]
 pub struct Vector3<T> {
     pub x: T,
@@ -5,9 +7,23 @@ pub struct Vector3<T> {
     pub z: T,
 }
 
-// TODO: we may have to specify method only for type int and float
+// TODO: implement other pub method
 impl<T> Vector3<T> {
     pub fn new(x: T, y: T, z: T) -> Self {
         Self { x, y, z }
+    }
+}
+
+// Operators
+impl<T> Index<usize> for Vector3<T> {
+    type Output = T;
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => panic!("out of bounds. Max index should be 2"), // TODO: any better way to handle
+                                                                 // this
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,8 @@ fn main() {
     let vec3: Vector3<i32> = Vector3::new(1, 2, 3);
 
     println!("{vec3:?}");
+    // checking index operator
+    println!("{}", vec3[0]);
+    println!("{}", vec3[1]);
+    println!("{}", vec3[2]);
 }
